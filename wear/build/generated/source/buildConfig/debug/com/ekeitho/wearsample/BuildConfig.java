1a8b1e47fde5039531b5e5f61e53245a7d2379d2
Keith Abdulla <email@ekeitho.com>
2014-09-14 19:03:41 -0700

    porting app to be wear-able

---
+++ wear/build/generated/source/buildConfig/debug/com/ekeitho/wearsample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ekeitho.wearsample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.ekeitho.wearsample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
